f8f6ea755e729acc4f125dc8976380b73fd8ffe0
ctibxe <hirotosuda1226@gmail.com>
2024-12-21 12:37:28 +0900

    表の作成

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>居酒屋  壱</h1>
+<body>
+ <table border="1" width="100%">
+  <tbody>
+    <tr>
+      <th>名前</th>
+      <th>人数</th>
+      <th>時間</th>
+      <th>詳細</th>
+      <th>電話番号</th>
+    </tr>
+    <tr v-for="i in 14" :key="i">
+      <td v-for="j in 5" :key="j">
+        &nbsp;
+      </td>
+    </tr>
+   </tbody>
+ </table>
+</body>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
