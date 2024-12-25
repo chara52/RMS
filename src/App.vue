e927e516d878513e75c1988a6cfc45288f6bca15
KANEKO Tomoki <s1300113@u-aizu.ac.jp>
2024-12-25 17:37:56 +0900

    Homeページ作成

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <script setup>
 </script>
 
+<!-- src/App.vue -->
 <template>
-  <h1>居酒屋  壱</h1>
-<body>
- <table border="1" width="100%">
-  <tbody>
-    <tr>
-      <th>名前</th>
-      <th>人数</th>
-      <th>時間</th>
-      <th>詳細</th>
-      <th>電話番号</th>
-    </tr>
-    <tr v-for="i in 14" :key="i">
-      <td v-for="j in 5" :key="j">
-        &nbsp;
-      </td>
-    </tr>
-   </tbody>
- </table>
-</body>
+  <router-view />
 </template>
 
-<style scoped>
+
+<style>
 </style>
+
--- src/main.js
+++ src/main.js
@@ -8,5 +8,8 @@ createApp(App).mount('#app')
 */
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
