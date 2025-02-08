f6670fd0fcdd98b1936e3893eb2bdcbc623b16a1
chara52 <yuto.3m52@icloud.com>
2025-02-08 12:58:04 +0900

    indexファイルの書き方統一

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import ReservationTable from '../components/ReservationTable.vue'
 import ReservationForm from '../components/ReservationForm.vue'
-import EditForm from '@/components/EditForm.vue'
+import EditForm from '../components/EditForm.vue'
 
 const router = createRouter({
   history: createWebHistory(),
