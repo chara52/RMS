cb8c61b25a00cd460178fe6225ef643e6d9ee148
ctibxe <hirotosuda1226@gmail.com>
2025-02-28 11:52:03 +0900

    PWAの導入と設定

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,50 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'RMS', // アプリケーションリスト等で表示される名前
+        short_name: 'RMS', // ホーム画面で表示される名前
+        description: '予約管理アプリです。予約内容を管理できます。',
+        theme_color: '#e6eeff', // テーマカラー最低限指定が必要
+        display: 'standalone', // 表示モード standaloneにするとネイティブアプリっぽくなる
+        icons: [ // 先ほどコンソールに出力されたものをコピペでOK
+          {
+            src: "/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
