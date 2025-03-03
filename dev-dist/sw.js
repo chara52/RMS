08ff602e57e9cb82cf96533aad471c10ca00a4ed
chara52 <yuto.3m52@icloud.com>
2025-03-03 13:14:20 +0900

    .envファイルにコード追加

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.29cqlh6iba8"
+    "revision": "0.mcg2h474b2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
