a95f887959de7fc2fa13cb42d5f92bb35b394326
ctibxe <hirotosuda1226@gmail.com>
2025-04-02 14:05:39 +0900

    Nuxtに移行

---
+++ components/EditReservation.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+
+// 編集画面に遷移し、idをURLに付与
+const editReservation = () => {
+  router.push({ path: '/EditForm', query: { id: props.id } })
+}
+</script>
+
+<template>
+  <button @click="editReservation" class="edit-button">
+    <i class="fa-solid fa-pen"></i>
+  </button>
+</template>
+
+<style scoped>
+.edit-button {
+  display: flex;
+  justify-content: center; /* 横方向の中央ぞろえ */
+  align-items: center; /* 縦方向の中央ぞろえ */
+  float: right;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: grey;
+  font-size: 1.5rem; /* アイコンサイズ */
+  width: 30px;
+  height: 30px;
+  margin-right: 15px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,54 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'RMS',
+      short_name: 'RMS',
+      theme_color: '#ffffff',
+      display: 'standalone', // 表示モード standaloneにするとネイティブアプリっぽくなる
+      icons: [
+        {
+          src: "/pwa-64x64.png",
+          sizes: "64x64",
+          type: "image/png"
+        },
+        {
+          src: "/pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "/pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        },
+        {
+          src: "/maskable-icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
+        }
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      navigateFallback: '/'
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  }
+})
