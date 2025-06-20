df8a762096cc94f8e8191773055dfbdb232ef66e
ctibxe <hirotosuda1226@gmail.com>
2025-06-20 10:29:16 +0900

    メニューボタンのUI変更

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -28,16 +28,29 @@ onUnmounted(() => {
   </button>
   <div ref="menuRef" class="menu" v-bind:class="{ 'is-active': isActive }">
     <router-link to="/HomePage" class="btn-link">
-      <div class="menu__item">ホーム</div>
+      <div class="menu__item">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        <i class="fa-solid fa-house"></i>
+        <span class="menu__text">ホーム</span>
+      </div>
     </router-link>
     <router-link to="/BeforeForm" class="btn-link">
-      <div class="menu__item">新規入力</div>
+      <div class="menu__item">
+        <i class="fa-solid fa-pen"></i>
+        <span class="menu__text">新規入力</span>
+      </div>
     </router-link>
     <router-link to="/ReservationTableCompact" class="btn-link">
-      <div class="menu__item">予約表</div>
+      <div class="menu__item">
+        <i class="fa-solid fa-table"></i>
+        <span class="menu__text">予約表</span>
+      </div>
     </router-link>
     <router-link to="/Shift" class="btn-link">
-      <div class="menu__item">シフト作成</div>
+      <div class="menu__item">
+        <i class="fa-solid fa-user-pen"></i>
+        <span class="menu__text">シフト作成</span>
+      </div>
     </router-link>
   </div>
 </template>
@@ -72,15 +85,52 @@ onUnmounted(() => {
   background: rgb(223, 220, 220);
   transform: translateX(100vw);
   transition: all .3s linear;
+  gap: 1em;
+    /* ボタン同士の間隔 */
+    padding: 3em 1em;
+    /* 全体の余白 */
+    box-sizing: border-box;
+    /* paddingを含めてサイズを計算 */
 }
 
 .menu__item{
-  width: 100%;
+
+  width: 70vw;
   height: auto;
-  padding: .5em 1em;
-  text-align: center;
+  font-size: 20px;
+  /*padding: .5em 1em;*/
+  text-align: left;
   color: black;
   box-sizing: border-box;
+  position: relative;
+  padding: .5em 1em .5em 3.5em;
+    /* 左にアイコン分のスペースを追加 */
+  display: flex;
+
+  /*
+  display: block;
+    width: 70vw;
+    max-width: 200px;
+    padding: 1em;
+    font-size: 20px;
+    text-align: center;
+    color: black;
+    border-radius: 8px;
+    text-decoration: none;
+    transition: background-color 0.2s ease;
+  */
+}
+
+.menu__item i.fa-solid {
+  position: relative;
+  width: 0.5em;
+  left: -40px; /* アイコンの移動 */
+  top: 5px; /* アイコンとテキストの上下を合わせる */
+}
+
+.menu__text {
+  display: inline-block;
+  flex: 100;
 }
 
 .menu.is-active{
