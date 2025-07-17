eda8110e9b458146f99edf0fcc0b48f6d0308d64
ctibxe <hirotosuda1226@gmail.com>
2025-07-17 15:34:54 +0900

    テストのためのプッシュ

--- pages/ReservationTableCompact.vue
+++ pages/ReservationTableCompact.vue
@@ -99,7 +99,7 @@ function handleSort(type, sortFunction) {
 
 <template>
   <div class="reservation-table-name">
-    <h1 class="global-h1">予約表</h1>
+    <h1 class="global-h1">予約表これはテストです</h1>
   </div>
 
   <div class="shift-info-container">
