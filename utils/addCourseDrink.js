3cdf5b42b34fced8a263fc637ddb26d99212aaa8
chara52 <yuto.3m52@icloud.com>
2025-07-19 10:30:17 +0900

    飲み放題が3時間の時に(3h)と表示

--- utils/addCourseDrink.js
+++ utils/addCourseDrink.js
@@ -16,10 +16,11 @@ export function addCourseDrink(course, drink) {
 
     if (trimmedDrink === "2500円（2h）") {
       DP = 2500;
+      return `${sumCP + DP}込`;
     } else if (trimmedDrink === "3000円（3h）") {
       DP = 3000;
+      return `${sumCP + DP}込(3h)`;
     }
-    return `${sumCP + DP}込`;
   } else if (Array.isArray(drink) && drink.length > 0 && course[0] === "なし" || course.length === 0) {
     const trimmedDrink = drink[0].trim();
 
